ab89e496162d9862c075bee9f854ae3d2273c23e
Axel <axeltm95@hotmail.com>
2021-03-11 21:06:48 -0600

    Color y secciones
    se agrego el color de zamna y se termino los popup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+
+ 
+
     extend: {
       fontFamily:{
         'Diamond' : ["Diamond", "sans-serif"],
         'Diamond-bold' : ['Diamond-bold', 'sans-serif-bold'],
         'Diamond-thin' : ['Diamond-thin' , 'sans-serif-thin']
+      },
+      colors: {
+        'Zamna-color' : '#34B8D5'
       }
     },
+
+   
   },
   variants: {
-    extend: {},
+    extend: { borderWidth: ['responsive', 'hover', 'focus'],},
   },
   plugins: [],
 }
